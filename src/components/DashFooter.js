2f2cab5a9ebc32a31306caa3a1f473b58457d61c
Your Name <davidnwachukwu3@gmail.com>
2023-10-11 13:45:36 +0100

    code refaction

--- src/components/DashFooter.js
+++ src/components/DashFooter.js
@@ -1,10 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHouse } from "@fortawesome/free-solid-svg-icons"
 import { useNavigate, useLocation } from "react-router-dom"
+import useAuth from "../hooks/useAuth"
 
 
 const DashFooter = () => {
     
+    const { username, status} = useAuth()
+
     const navigate = useNavigate()
 
     const { pathname } = useLocation()
@@ -30,8 +33,8 @@ const DashFooter = () => {
         
         <footer className="dash-footer">
             {goHomeButton}
-            <p> Current User:</p>
-            <p> Status:</p>
+            <p> Current User: {username}</p>
+            <p> Status: {status}</p>
         </footer>
     )
   
--- src/features/auth/Welcome.js
+++ src/features/auth/Welcome.js
@@ -1,23 +1,28 @@
 import {Link} from "react-router-dom"
+import useAuth from "../../hooks/useAuth"
 
-const date = new Date()
-const today = new Intl.DateTimeFormat('en-US', {dateStyle: 'full',timeStyle: 'long'}).format(date)
 
 const Welcome = () => {
 
+   const { username, isManager, isAdmin} = useAuth()
+
+  const date = new Date()
+  const today = new Intl.DateTimeFormat('en-US', {dateStyle: 'full',timeStyle: 'long'}).format(date)
+  
+    
     const content = (
        <section className="welcome"> 
         <p>{today}</p>
 
-        <h1>welcome!</h1>
+        <h1>welcome {username}!</h1>
 
         <p><Link to="/dash/notes">View techNotes</Link></p>
 
         <p><Link to="/dash/notes/new">Add New techNote</Link></p>
 
-        <p><Link to="/dash/users">View User Settings</Link></p>
+        {(isManager || isAdmin) && <p><Link to="/dash/users">View User Settings</Link></p>}
 
-        <p><Link to="/dash/users/new">Add New User</Link></p>
+        {(isManager || isAdmin) && <p><Link to="/dash/users/new">Add New User</Link></p>}
         
        </section>
     )
